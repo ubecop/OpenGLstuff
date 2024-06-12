a42ca24536912fe4aa135bde0f5fc57aaed7235a
Matthew <matthew.sanchez@dorea.fr>
2024-06-12 15:42:57 +0200

    Added comments

--- YoutubeOpenGL/Texture.cpp
+++ YoutubeOpenGL/Texture.cpp
@@ -2,28 +2,42 @@
 
 Texture::Texture(const char* image, GLenum texType, GLenum slot, GLenum format, GLenum pixelType)
 {
+	// Assigns the type of the texture to the texture object
 	type = texType;
+
+	// Stores the width, height, and number of color channels on the image
 	int widthImg, heightImg, numColCh;
+	// Flips the image so it appears right side up
 	stbi_set_flip_vertically_on_load(true);
+	// Reads the image from a file and stores in bytes
 	unsigned char* bytes = stbi_load(image, &widthImg, &heightImg, &numColCh, 0);
 
+	// Generates an OpenGL texture object
 	glGenTextures(1, &ID);
+	// Assigns the texture to a Texture Unit
 	glActiveTexture(slot);
 	glBindTexture(texType, ID);
 
+	// Configures the type of algorithm that is used to make the image smaller/bigger
 	glTexParameteri(texType, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 	glTexParameteri(texType, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
+	// Configures the way the texture repeats (or doesn't)
 	glTexParameteri(texType, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(texType, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
+	// To use in case you replace GL_REPEAT with GL_CLAMP_TO_BORDER
 	// float flatColor[] = {1.0f, 1.0f, 1.0f, 1.0f}
 	// glTexParameterfv(texType, GL_TEXTURE_BORDER_COLOR, flatColor);
 
+	// Assigns the image to the OpenGL Texture Object
 	glTexImage2D(texType, 0, GL_RGBA, widthImg, heightImg, 0, format, pixelType, bytes);
+	// Generate MipMaps (smaller resolutions of the same picture)
 	glGenerateMipmap(texType);
 
+	// Deletes image data as it's already in the OpenGL Texture object
 	stbi_image_free(bytes);
+	// Unbinds the OpenGL Texture object so that it can't accidentally be modified
 	glBindTexture(texType, 0);
 }
 
