89d4a3f5444a1495887c3be089cdd5a6545ad44f
Matthew <matthew.sanchez@dorea.fr>
2024-06-12 14:55:45 +0200

    Fixed missing file error

--- YoutubeOpenGL/Main.cpp
+++ YoutubeOpenGL/Main.cpp
@@ -1,7 +1,10 @@
 #include<iostream>
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
+#include<stb\stb_image.h>
 
+
+#include"Texture.h"
 #include"shaderClass.h"
 #include"VAO.h"
 #include"VBO.h"
@@ -11,21 +14,18 @@
 
 // Vertices coordinates
 GLfloat vertices[] =
-{ //     COORDINATES                          /   COLORS               //
-	-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,       0.8f, 0.3f, 0.02f,  // Lower left corner
-	 0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,       0.8f, 0.3f, 0.02f,  // Lower right corner
-	 0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f,    1.0f, 0.6f, 0.32f,  // Upper corner
-	-0.25f, 0.5f * float(sqrt(3)) / 6, 0.0f,       0.9f, 0.45f,0.17f,  // Inner left
-	 0.25f, 0.5f * float(sqrt(3)) / 6, 0.0f,       0.9f, 0.45f,0.17f,  // Inner right
-	 0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f,       0.8f, 0.3f, 0.02f   // Inner down
+{ //     COORDINATES    /   COLORS         // TEXTURES    
+	-0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   0.0f, 0.0f,// Lower left corner
+	-0.5f,  0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   0.0f, 1.0f,// Upper left corner
+	 0.5f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f,   1.0f, 1.0f,// Upper right corner
+	 0.5f, -0.5f, 0.0f,  1.0f, 1.0f, 1.0f,   1.0f, 0.0f // Lower left corner
 };
 
 // Indices for vertices order
 GLuint indices[] =
 {
-	0, 3, 5, // Lower left triangle
-	3, 2, 4, // Lower right triangle
-	5, 4, 1 // Upper triangle
+	0, 2, 1, // Upper triangle
+	0, 3, 2  // Lower triangle
 };
 
 
@@ -78,8 +78,9 @@ int main()
 	EBO EBO1(indices, sizeof(indices));
 
 	// Links VBO to VAO
-	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 6 * sizeof(float), (void*) 0);
-	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 8 * sizeof(float), (void*)0);
+	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	VAO1.LinkAttrib(VBO1, 2, 2, GL_FLOAT, 8 * sizeof(float), (void*)(6 * sizeof(float)));
 	// Unbind all to prevent accidentally modifying them
 	VAO1.Unbind();
 	VBO1.Unbind();
@@ -88,48 +89,30 @@ int main()
 	// Get ID of uniform called "scale"
 	GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
 
+	// Texture
 	
-	//float prev_time = float(glfwGetTime()); // Keeps track of time
-	//float angle = 0.0f; // Will control the color
-	//
+	Texture popCat("pop_cat.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
+	popCat.texUnit(shaderProgram, "tex0", 0);
 
 	// Main while loop
 	while (!glfwWindowShouldClose(window))
 	{
-		
+
 		// Specify the color of the background
 		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
 		// Clean the back buffer and assign the new color to it
 		glClear(GL_COLOR_BUFFER_BIT);
 		// Tell OpenGL which Shader Program we want to use
 		shaderProgram.Activate();
+		// Assigns a value to the uniform; NOTE: Must always be done after activating the Shader Program
 		glUniform1f(uniID, 0.5f);
+		popCat.Bind();
 		// Bind the VAO so OpenGL knows to use it
 		VAO1.Bind();
 		// Draw primitives, number of indices, datatype of indices, index of indices
-		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		// Swap the back buffer with the front buffer
 		glfwSwapBuffers(window);
-		//glfwSwapBuffers(window); //If you want an epileptic seizure
-		
-
-		// To use this trigonometry section, remove the comment markers from prev_time and angle
-		// And put the section of the loop above in comments 
-
-		
-		//float time = float(glfwGetTime());
-		//if (time - prev_time >= 0.1f) // this is True every 0.1 seconds
-		//{
-		//	angle += 0.1f; // Changes angle to change colors
-		//	prev_time = time; // Resets prev_time to current time
-		//}
-
-		//// Use of trigonometry to nicely change colors
-		//glClearColor(float(sin(angle)), float(cos(angle)), float(tan(angle)), 1.0f);
-		//// Basic needed functions for changes to be visible
-		//glClear(GL_COLOR_BUFFER_BIT);
-		//glfwSwapBuffers(window);
-		
 
 		// Take care of all GLFW events
 		glfwPollEvents();
@@ -141,6 +124,7 @@ int main()
 	VAO1.Delete();
 	VBO1.Delete();
 	EBO1.Delete();
+	popCat.Delete();
 	shaderProgram.Delete();
 	// Delete window before ending the program
 	glfwDestroyWindow(window);
--- YoutubeOpenGL/default.frag
+++ YoutubeOpenGL/default.frag
@@ -1,6 +1,18 @@
 #version 330 core
+
+// Outputs colors in RGBA
 out vec4 FragColor;
+
+// Inputs the color from the Vertex Shader
+in vec3 color;
+
+// Inputs texture coordinates from Vertex Shader
+in vec2 texCoord;
+
+// Gets Texture Unit from the main function
+uniform sampler2D tex0;
+
 void main()
 {
-   FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);
+   FragColor = texture(tex0, texCoord);
 }
